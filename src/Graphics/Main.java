44a2b3ee496444011864f5cba82cecdf949d9e75
Hazem Jday <hazemjday@gmail.com>
2022-04-24 17:00:21 +0100

    Add GUI

--- src/Graphics/Main.java
+++ src/Graphics/Main.java
@@ -31,12 +31,7 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		new Frame().setVisible(true);
 	}
 
 }
